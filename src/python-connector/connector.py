67e9eb545eafa7ca44b6c12cf2578d5ca11f5989
root <root@62b9bc05151e>
2026-02-26 07:48:21 +0000

    fix: update connector for API Qualis

--- src/python-connector/connector.py
+++ src/python-connector/connector.py
@@ -138,13 +138,13 @@ class QualysAWSConnector:
         return AWSConnector(
             name=data.get("name", ""),
             # Using 'connector_id' but API returns 'connectorId'
-            connector_id=data.get("connector_id", data.get("connectorId", "")),
+            connector_id=data.get("connectorId", data.get("connectorId", "")),
             description=data.get("description", ""),
             provider=data.get("provider", ""),
             # Using 'status' but API returns 'state' (FIELD RENAMED)
             status=data.get("status", data.get("state", "")),
             # Using 'total_assets' but API returns 'totalAssets'
-            total_assets=data.get("total_assets", data.get("totalAssets", 0)),
+            total_assets=data.get("totalAssets", data.get("totalAssets", 0)),
             # Using 'last_synced_on' but API returns 'lastSyncedOn'
             last_synced_on=data.get("last_synced_on", data.get("lastSyncedOn", "")),
             # Using 'is_gov_cloud' but API returns 'isGovCloud'
